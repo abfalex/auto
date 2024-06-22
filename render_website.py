3ec95ff01b3f4e53ded02bf87779f11a28d64eee
abfalex <87718997+abfalex@users.noreply.github.com>
2024-06-22 12:03:36 +0300

    Обновил рендер шаблонов

--- render_website.py
+++ render_website.py
@@ -1,4 +1,5 @@
 import json
+import os
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
@@ -8,12 +9,32 @@ def on_reload():
         loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
     )
 
-    template = env.get_template("template.html")
+    main_template = env.get_template("template.html")
+    page_template = env.get_template("page_template.html")
+    brands_folder = "pages"
+
+    with open("brands.json", "r", encoding="UTF-8") as f:
+        brands = json.load(f)
 
     with open("cars.json", "r", encoding="UTF-8") as f:
-        cars = json.load(f)
+        automobiles = json.load(f)
 
-    rendered_page = template.render(brands=cars["marks"], cars=cars["cars"])
+    cars_on_page = 20
+
+    for index, brand in enumerate(brands, start=1):
+        brand_folder = f"{brand['brand_name']}"
+        page_filename = f"page{index}.html"
+        page_output_path = os.path.join(brands_folder, brand_folder, page_filename)
+        os.makedirs(f"{brands_folder}/{brand_folder}", exist_ok=True)
+        for automobile in automobiles:
+            rendered_car_page = page_template.render(
+                brand=brand, automobiles=automobiles
+            )
+
+            with open(page_output_path, "w", encoding="UTF-8") as f:
+                f.write(rendered_car_page)
+
+    rendered_page = main_template.render(brands=brands)
 
     with open("index.html", "w", encoding="UTF-8") as f:
         f.write(rendered_page)
