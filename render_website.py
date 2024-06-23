6f5d3d8f60b3c798e6d3cb65a24bf2df27d0d087
abfalex <87718997+abfalex@users.noreply.github.com>
2024-06-23 20:29:03 +0300

    Обновил рендер

--- render_website.py
+++ render_website.py
@@ -1,50 +1,66 @@
 import json
 import os
+import pprint
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
+from more_itertools import chunked
+
+
+def format_price(value):
+    return f"{value:,}".replace(",", " ")
 
 
 def on_reload():
     env = Environment(
-        loader=FileSystemLoader("."), autoescape=select_autoescape(["html", "xml"])
+        loader=FileSystemLoader("."),
+        autoescape=select_autoescape(["html", "xml"]),
     )
+    env.filters["format_price"] = format_price
 
     main_template = env.get_template("template.html")
     page_template = env.get_template("page_template.html")
-    brands_folder = "pages"
+    pages_folder = "pages"
 
     with open("brands.json", "r", encoding="UTF-8") as f:
         brands = json.load(f)
 
-    with open("cars.json", "r", encoding="UTF-8") as f:
-        automobiles = json.load(f)
-
     cars_on_page = 20
 
-    for index, brand in enumerate(brands, start=1):
-        brand_folder = f"{brand['brand_name']}"
-        os.makedirs(f"{brands_folder}/{brand_folder}", exist_ok=True)
+    for brand_number, brand in enumerate(brands, start=1):
+        brand_folder_name = f"{brand['brand_name']}"
+        brand_page_path = os.path.join(pages_folder, brand_folder_name)
 
-        for auto_index, automobile in enumerate(automobiles, start=1):
-            if automobile["brand"] != brand["brand_name"]:
-                continue
-            print(automobile)
+        os.makedirs(brand_page_path, exist_ok=True)
 
-            page_filename = f"page{auto_index}.html"
-            page_output_path = os.path.join(brands_folder, brand_folder, page_filename)
-            rendered_car_page = page_template.render(
-                brand=brand, automobile=automobile, page_number=index
-            )
+        brands_folder = os.path.join("brands")
+        brand_folder_path = os.path.join(brands_folder, brand_folder_name)
+        for brand_file in os.listdir(brand_folder_path):
+            with open(f"{brand_folder_path}/{brand_file}", "r", encoding="UTF-8") as f:
+                automobiles = json.load(f)
+                auto_on_page = automobiles[0:cars_on_page]
 
-            with open(page_output_path, "w", encoding="UTF-8") as f:
-                f.write(rendered_car_page)
+            page_number = list(chunked(automobiles, 20))
+            for page_number, automobiles in enumerate(page_number, start=1):
 
-            rendered_page = main_template.render(
-                brands=brands,
-                page_number=index,
-                automobiles=automobiles,
-                total_pages=len(brands),
-            )
+                page_filename = f"page{page_number}.html"
+                page_output_path = os.path.join(brand_page_path, page_filename)
+
+                rendered_car_page = page_template.render(
+                    brands=brands,
+                    brand=brand,
+                    automobiles=automobiles,
+                    page_number=brand_number,
+                )
+
+                with open(page_output_path, "w", encoding="UTF-8") as f:
+                    f.write(rendered_car_page)
+
+                rendered_page = main_template.render(
+                    brands=brands,
+                    page_number=brand_number,
+                    automobiles=auto_on_page,
+                    total_pages=len(brands),
+                )
 
     with open("index.html", "w", encoding="UTF-8") as f:
         f.write(rendered_page)
@@ -54,6 +70,8 @@ def main():
     on_reload()
     server = Server()
     server.watch("template.html", on_reload)
+    server.watch("page_template.html", on_reload)
+    server.watch("base_template.html", on_reload)
     server.serve(root=".", default_filename="index.html")
 
 
